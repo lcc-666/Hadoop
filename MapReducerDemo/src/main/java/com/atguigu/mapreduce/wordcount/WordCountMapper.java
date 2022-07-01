c762e9caa72d44a2d0fac76d517044ce29a37418
lcc-666 <2502247952@qq.com>
2022-07-01 23:19:35 +0800

    编写mapper

--- MapReducerDemo/src/main/java/com/atguigu/mapreduce/wordcount/WordCountMapper.java
+++ MapReducerDemo/src/main/java/com/atguigu/mapreduce/wordcount/WordCountMapper.java
@@ -1,4 +1,31 @@
 package com.atguigu.mapreduce.wordcount;
 
-public class WordCountMapper {
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+
+public class WordCountMapper extends Mapper<LongWritable,Text,Text, IntWritable> {
+    private Text outK=new Text();
+    private IntWritable outV=new IntWritable(1);
+
+
+    @Override
+    protected void map(LongWritable key, Text value, Mapper<LongWritable, Text, Text, IntWritable>.Context context) throws IOException, InterruptedException {
+        String line = value.toString();
+
+        String[] words = line.split(" ");
+
+        for (String word : words) {
+
+            outK.set(word);
+
+            context.write(outK,outV);
+        }
+
+        
+
+    }
 }
