a87c8ae824be7469daeba6783bbd6105cd91f138
lcc-666 <2502247952@qq.com>
2022-07-02 11:44:21 +0800

    编写driver

--- MapReducerDemo/src/main/java/com/atguigu/mapreduce/wordcount/WordCountDriver.java
+++ MapReducerDemo/src/main/java/com/atguigu/mapreduce/wordcount/WordCountDriver.java
@@ -1,4 +1,39 @@
 package com.atguigu.mapreduce.wordcount;
 
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.reduce.WrappedReducer;
+
+import java.io.IOException;
+
 public class WordCountDriver {
+    public static void main(String[] args) throws IOException, InterruptedException, ClassNotFoundException {
+        Configuration conf = new Configuration();
+        Job job = Job.getInstance(conf);
+
+        job.setJarByClass(WordCountDriver.class);
+
+        job.setMapperClass(WordCountMapper.class);
+        job.setReducerClass(WrappedReducer.class);
+
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(IntWritable.class);
+
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+
+        FileInputFormat.setInputPaths(job,new Path("D:\\learn\\hadoop\\wordcount\\input"));
+        FileOutputFormat.setOutputPath(job,new Path("D:\\learn\\hadoop\\wordcount\\output"));
+
+        boolean result = job.waitForCompletion(true);
+
+        System.exit(result?0:1);
+    }
+
+
 }
