e9fcdd3cf2156e45961d9d7bbbc4779dddc865c8
lcc-666 <2502247952@qq.com>
2022-07-04 11:46:34 +0800

    序列化

---
+++ MapReducerDemo/src/main/java/com/atguigu/mapreduce/writable/FlowBean.java
@@ -0,0 +1,64 @@
+package com.atguigu.mapreduce.writable;
+
+import org.apache.hadoop.io.Writable;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+public class FlowBean implements Writable {
+    private long upFlow;
+    private long downFlow;
+    private long sumFlow;
+
+
+    public FlowBean() {
+    }
+
+    public long getUpFlow() {
+        return upFlow;
+    }
+
+    public void setUpFlow(long upFlow) {
+        this.upFlow = upFlow;
+    }
+
+    public long getDownFlow() {
+        return downFlow;
+    }
+
+    public void setDownFlow(long downFlow) {
+        this.downFlow = downFlow;
+    }
+
+    public long getSumFlow() {
+        return sumFlow;
+    }
+
+    public void setSumFlow(long sumFlow) {
+        this.sumFlow = sumFlow;
+    }
+
+    public void setSumFlow() {
+        this.sumFlow = this.upFlow + this.downFlow;
+    }
+
+    @Override
+    public void write(DataOutput dataOutput) throws IOException {
+        dataOutput.writeLong(upFlow);
+        dataOutput.writeLong(downFlow);
+        dataOutput.writeLong(sumFlow);
+    }
+
+    @Override
+    public void readFields(DataInput dataInput) throws IOException {
+        this.upFlow=dataInput.readLong();
+        this.downFlow=dataInput.readLong();
+        this.sumFlow=dataInput.readLong();
+    }
+
+    @Override
+    public String toString() {
+        return  upFlow + "\t" + downFlow + "\t" + sumFlow;
+    }
+}
---
+++ MapReducerDemo/src/main/java/com/atguigu/mapreduce/writable/FlowDriver.java
@@ -0,0 +1,34 @@
+package com.atguigu.mapreduce.writable;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+import java.io.IOException;
+
+public class FlowDriver {
+    public static void main(String[] args) throws IOException, InterruptedException, ClassNotFoundException {
+        Configuration conf = new Configuration();
+        Job job = Job.getInstance(conf);
+
+        job.setJarByClass(FlowDriver.class);
+
+        job.setMapperClass(FlowMapper.class);
+        job.setReducerClass(FlowReducer.class);
+
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(FlowBean.class);
+
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(FlowBean.class);
+
+        FileInputFormat.setInputPaths(job, new Path("D:\\learn\\hadoop\\writable\\input"));
+        FileOutputFormat.setOutputPath(job, new Path("D:\\learn\\hadoop\\writable\\output"));
+
+        boolean result = job.waitForCompletion(true);
+        System.exit(result ? 0 : 1);
+    }
+}
