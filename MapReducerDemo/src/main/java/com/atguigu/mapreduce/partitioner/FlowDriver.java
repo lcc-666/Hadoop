d22fa652e6b04e235e8d8958d1c24aa4e1d14b7d
lcc-666 <2502247952@qq.com>
2022-07-07 11:22:32 +0800

    partitioner分区

---
+++ MapReducerDemo/src/main/java/com/atguigu/mapreduce/partitioner/FlowDriver.java
@@ -0,0 +1,37 @@
+package com.atguigu.mapreduce.partitioner;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+import java.io.IOException;
+
+public class FlowDriver {
+    public static void main(String[] args) throws IOException, InterruptedException, ClassNotFoundException {
+        Configuration conf = new Configuration();
+        Job job = Job.getInstance(conf);
+
+        job.setJarByClass(FlowDriver.class);
+
+        job.setMapperClass(FlowMapper.class);
+        job.setReducerClass(FlowReducer.class);
+
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(FlowBean.class);
+
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(FlowBean.class);
+
+        job.setPartitionerClass(ProvincePartitioner.class);
+        job.setNumReduceTasks(5);
+
+        FileInputFormat.setInputPaths(job, new Path("D:\\learn\\hadoop\\writable\\input"));
+        FileOutputFormat.setOutputPath(job, new Path("D:\\learn\\hadoop\\writable\\output1"));
+
+        boolean result = job.waitForCompletion(true);
+        System.exit(result ? 0 : 1);
+    }
+}
