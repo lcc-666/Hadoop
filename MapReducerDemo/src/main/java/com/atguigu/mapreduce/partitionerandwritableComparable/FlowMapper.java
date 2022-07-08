065688d04f87b338b3f931f345fd73c87e75961b
lcc-666 <2502247952@qq.com>
2022-07-08 17:51:39 +0800

    区内排序

---
+++ MapReducerDemo/src/main/java/com/atguigu/mapreduce/partitionerandwritableComparable/FlowMapper.java
@@ -0,0 +1,27 @@
+package com.atguigu.mapreduce.partitionerandwritableComparable;
+
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+
+public class FlowMapper extends Mapper<LongWritable, Text, FlowBean, Text> {
+    private FlowBean outK = new FlowBean();
+    private Text outV = new Text();
+
+
+    @Override
+    protected void map(LongWritable key, Text value, Mapper<LongWritable, Text, FlowBean, Text>.Context context) throws IOException, InterruptedException {
+        String line = value.toString();
+
+        String[] split = line.split("\t");
+
+        outV.set(split[0]);
+        outK.setUpFlow(Long.parseLong(split[1]));
+        outK.setDownFlow(Long.parseLong(split[2]));
+        outK.setSumFlow();
+
+        context.write(outK,outV);
+    }
+}
